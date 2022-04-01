40ae82772852edc39b7c41e1595e676dc550280f
Eli <yuchien.elliot@gmail.com>
2022-04-01 16:02:31 -0700

    updated

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1, 100)
-
+count = 0 # counting times of guess
 while True:
+	count += 1  # count = count + 1
 	num = input('Please take a guess of a random number: ')
 	num = int(num)
 	if num == r:
 		print('Bingo!')
+		print('This is your ', count, 'attempt(s).')
 		break
 	elif num > r:
 		print('Your answer is greater than the answer.')
 	elif num < r:
 		print('Your answer is lower than the answer.')
+	print('This is your ', count, 'attempt(s).')
